61c238cd7f9a8de4fd3c8b61282ffd9bd67dc864
Filipe Pina <fopina@gmail.com>
2021-02-19 00:33:16 +0000

    build for 1.7.0

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ docker build -f Dockerfile.build -o dist .
 docker run --rm alpine:3.12 cat /proc/meminfo
 docker run --rm \
            -v $(pwd)/dist:/myplugin \
-           fluent/fluent-bit:1.6.10 \
+           fluent/fluent-bit:1.7.0 \
            /fluent-bit/bin/fluent-bit -v \
            -f 1 \
            -e /myplugin/flb-in_mem2.so \
